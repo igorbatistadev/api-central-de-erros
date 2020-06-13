13e93b4e3d3f9f2d54739fedaa0e9b6d9d758aba
Igor Batista <igorbatista.dev@gmail.com>
2020-06-12 23:27:43 -0300

    Commit inicial do projeto

---
+++ src/test/java/com/codenation/central/api/CentralErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenation.central.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
