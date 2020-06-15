dec0f1c888a9aae587e850021edeeb729c9ac1bd
Igor Batista <igorbatista.dev@gmail.com>
2020-06-15 20:19:43 -0300

    Adiciona configuração da Auditoria JPA

--- src/main/java/com/codenation/central/api/model/Usuario.java
+++ src/main/java/com/codenation/central/api/model/Usuario.java
@@ -1,12 +1,15 @@
 package com.codenation.central.api.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario {
 
     @Id
@@ -15,6 +18,7 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
+    @CreatedDate
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
 
