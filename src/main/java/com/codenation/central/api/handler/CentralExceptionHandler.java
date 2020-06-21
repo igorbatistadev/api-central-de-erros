205503b57e2b1bff2e1040a59a700b427084e858
Igor Batista <igorbatista.dev@gmail.com>
2020-06-20 23:40:53 -0300

    Adiciona mais métodos para tratamento de exceções

--- src/main/java/com/codenation/central/api/handler/CentralExceptionHandler.java
+++ src/main/java/com/codenation/central/api/handler/CentralExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.codenation.central.api.handler;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -32,7 +34,42 @@ public class CentralExceptionHandler extends ResponseEntityExceptionHandler {
                                                                   HttpStatus status, WebRequest request) {
         List<ValidationObject> errors = getErrors(ex);
         ErrorValidation errorValidation = getErrorValidation(ex, status, errors);
-        return new ResponseEntity<>(errorValidation, status);
+        return new ResponseEntity<>(errorValidation, headers, status);
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    protected ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .title("Violação de restrições")
+                .status(HttpStatus.CONFLICT.value())
+                .detail(ex.getRootCause().getMessage())
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .title("Erro interno")
+                .status(status.value())
+                .detail(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(errorResponse, headers, status);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .title("JSON mal formatado")
+                .status(status.value())
+                .detail(ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(errorResponse, headers, status);
     }
 
     private ErrorValidation getErrorValidation(MethodArgumentNotValidException ex, HttpStatus status, List<ValidationObject> errors) {
